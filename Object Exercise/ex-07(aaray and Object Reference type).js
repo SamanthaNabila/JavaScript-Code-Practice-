a53a29929c335508219f28361d6b10f8bcddf5b0
Jannatul Ferdous Nabila <samanthanabila47@gmail.com>
2026-04-27 13:25:50 +0600

    Array & Object (reference type) exercise

---
+++ Object Exercise/ex-07(aaray and Object Reference type).js
@@ -0,0 +1,5 @@
+const arr1 = [1, 2, 3];
+const arr2 = arr1;
+arr2.push(4);
+console.log(arr1);
+//spred operator diye copy kore nite hobe deep copye jonno. arr2=arr1 means arra obhject type so arr2 actuall arr1 er referance indictae kore so arr2 change korle arr1 one o change hoye jabe
